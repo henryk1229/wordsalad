08c9f4644c664399dba13dc424c8e319f6ab0d12
henryk1229 <henryk1229@gmail.com>
2024-01-09 15:40:53 -0500

    feat: add headers to webpack config

--- apps/api/webpack.config.js
+++ apps/api/webpack.config.js
@@ -4,5 +4,6 @@ const { composePlugins, withNx } = require('@nx/webpack');
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
   // e.g. `config.plugins.push(new MyPlugin())`
+  config.headers = { 'Access-Control-Allow-Origin': '*' };
   return config;
 });
