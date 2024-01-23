7021c78dc8191ee4b05fc43e81424a2e103d901e
henryk1229 <henryk1229@gmail.com>
2024-01-23 18:52:02 -0500

    feat: improved cron job

--- apps/api/src/cron-job.ts
+++ apps/api/src/cron-job.ts
@@ -2,16 +2,36 @@ import cron from 'node-cron';
 import axios from 'axios';
 import config from './config';
 
-export const cronJob = cron.schedule('* 0 * * 0-6', () => {
-  axios
-    .post(`${config.apiUrl}/generate-salad`)
-    .then((response) => {
-      console.log('GENERATED SALAD:', response?.data?.initialWord);
-    })
-    .catch((error) => {
-      console.log(`COULD NOT GENERATE SALAD: ${error.message}`);
-    }),
+const SCHEDULE = '* 0 * * 0-6';
+
+export const scheduleCronAndLogs = () => {
+  const isValid = cron.validate(SCHEDULE);
+
+  if (!isValid) {
+    throw new Error('INVALID CRON SCHEDULE');
+  }
+
+  cron.schedule(
+    SCHEDULE,
+    () => {
+      axios
+        .post(`${config.apiUrl}/generate-salad`)
+        .then((response) => {
+          console.log('GENERATED SALAD:', response?.data?.initialWord);
+        })
+        .catch((error) => {
+          console.log(`COULD NOT GENERATE SALAD: ${error.message}`);
+        });
+    },
     {
       name: 'generate-salad',
-    };
-});
+      timezone: 'America/New_York',
+    }
+  );
+
+  const tasks = cron.getTasks();
+
+  for (const [key] of tasks.entries()) {
+    console.log('SCHEDULED TASK:', key);
+  }
+};
